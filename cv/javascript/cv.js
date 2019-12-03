a311d1f443939003d2a1a5698ab58b96744a7d2d
murielglumineau <57603234+murielglumineau@users.noreply.github.com>
2019-12-03 11:52:10 +0100

    Add files via upload

---
+++ cv/javascript/cv.js
@@ -0,0 +1,10 @@
+document.addEventListener('keydown', function(e){
+    if(event.code =='keyC'||'keyV'){
+        alert("N'oubliez pas de télécharger mon CV");
+        document.getElementById('cv')
+    };
+    
+});
+
+
+
